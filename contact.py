0628d2604b60a3a189ee9e8a7d3b9e809612d544
AlexeyKozlov <alexeykozlov@Alexeys-MBP.westell.com>
2016-10-17 15:56:21 -0400

    Created 2 tests: create empty and filled contact

---
+++ contact.py
@@ -0,0 +1,13 @@
+
+class Contact:
+
+    def __init__(self, Name, Lastname, Company, Title, City, State, Zip, Country, Phone):
+        self.Name=Name
+        self.Lastname=Lastname
+        self.Company=Company
+        self.Title=Title
+        self.City=City
+        self.State=State
+        self.Zip=Zip
+        self.Country=Country
+        self.Phone=Phone
--- test_add_contacts.py
+++ test_add_contacts.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
-
+import unittest
+from contact import Contact
 def is_alert_present(wd):
     try:
-        wd.switch_to_alert().text
+        wd.switch_to_alert.text
         return True
     except:
         return False
@@ -16,64 +15,93 @@ class contacts(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_contacts(self):
-        success = True
         wd = self.wd
-        wd.get("https://www.keepandshare.com/business/logout.php?i=2683987")
-        wd.find_element_by_id("username").click()
-        wd.find_element_by_id("username").clear()
-        wd.find_element_by_id("username").send_keys("kozlov")
-        wd.find_element_by_id("password").click()
-        wd.find_element_by_id("password").clear()
-        wd.find_element_by_id("password").send_keys("Kozlov30031988")
-        wd.find_element_by_xpath("//form[@id='signinbox']//button[.=' Secure Sign In']").click()
-        wd.find_element_by_link_text("Addresses").click()
+        self.open_home_page(wd)
+        self.login(wd, username="kozlov", password="Kozlov30031988")
+        self.open_adresses(wd)
+        self.create_contact(wd, Contact(Name="Wei", Lastname="Mei", Company="weimei inc.", Title="director", City="Somerville", State="MA",
+                            Zip="02145", Country="USA",
+                            Phone="8573182639"))
+        self.return_to_contact_page(wd)
+        self.logout(wd)
+
+
+    def test_add_empty_contacts(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="kozlov", password="Kozlov30031988")
+        self.open_adresses(wd)
+        self.create_contact(wd, Contact(Name="Empty", Lastname="Empty", Company="Empty", Title="Empty", City="", State="",
+                            Zip="", Country="",
+                            Phone=""))
+        self.return_to_contact_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_id("anonymous_element_2").click()
+
+    def return_to_contact_page(self, wd):
+        wd.find_element_by_xpath("//div[@class='docEditButtonbar']//button[.='Save and Exit']").click()
+
+    def create_contact(self, wd, contact):
+        # init new contact
         wd.find_element_by_id("cmdItemNew").click()
+        # fill contact form
         wd.find_element_by_id("fname").click()
         wd.find_element_by_id("fname").clear()
-        wd.find_element_by_id("fname").send_keys("Wei")
+        wd.find_element_by_id("fname").send_keys(contact.Name)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Mei")
-        wd.find_element_by_id("ABTabName1").click()
-        wd.find_element_by_id("ABTabName1").clear()
-        wd.find_element_by_id("ABTabName1").send_keys("Weimei")
+        wd.find_element_by_name("title").send_keys(contact.Lastname)
         wd.find_element_by_id("ABTabCompany1").click()
         wd.find_element_by_id("ABTabCompany1").clear()
-        wd.find_element_by_id("ABTabCompany1").send_keys("weimei inc.")
+        wd.find_element_by_id("ABTabCompany1").send_keys(contact.Company)
         wd.find_element_by_id("ABTabTitle1").click()
         wd.find_element_by_id("ABTabTitle1").clear()
-        wd.find_element_by_id("ABTabTitle1").send_keys("director")
+        wd.find_element_by_id("ABTabTitle1").send_keys(contact.Title)
         if not wd.find_element_by_xpath("//select[@id='ABTabAddrType1']//option[1]").is_selected():
             wd.find_element_by_xpath("//select[@id='ABTabAddrType1']//option[1]").click()
         wd.find_element_by_id("ABTabAddr11").click()
-        wd.find_element_by_id("ABTabAddr11").send_keys("\\9")
+        wd.find_element_by_id("ABTabAddr11").send_keys("")
         wd.find_element_by_id("ABTabAddr21").click()
-        wd.find_element_by_id("ABTabAddr21").send_keys("\\9")
+        wd.find_element_by_id("ABTabAddr21").send_keys("")
         wd.find_element_by_id("ABTabCity1").click()
         wd.find_element_by_id("ABTabCity1").clear()
-        wd.find_element_by_id("ABTabCity1").send_keys("Somerville")
+        wd.find_element_by_id("ABTabCity1").send_keys(contact.City)
         wd.find_element_by_id("ABTabState1").click()
         wd.find_element_by_id("ABTabState1").clear()
-        wd.find_element_by_id("ABTabState1").send_keys("MA")
+        wd.find_element_by_id("ABTabState1").send_keys(contact.State)
         wd.find_element_by_id("ABTabZip1").click()
         wd.find_element_by_id("ABTabZip1").clear()
-        wd.find_element_by_id("ABTabZip1").send_keys("02145")
+        wd.find_element_by_id("ABTabZip1").send_keys(contact.Zip)
         wd.find_element_by_id("ABTabCountry1").click()
         wd.find_element_by_id("ABTabCountry1").clear()
-        wd.find_element_by_id("ABTabCountry1").send_keys("USA")
+        wd.find_element_by_id("ABTabCountry1").send_keys(contact.Country)
         if not wd.find_element_by_xpath("//select[@id='ABTabPhone1Type1']//option[1]").is_selected():
             wd.find_element_by_xpath("//select[@id='ABTabPhone1Type1']//option[1]").click()
         wd.find_element_by_id("ABTabPhone11").click()
         wd.find_element_by_id("ABTabPhone11").clear()
-        wd.find_element_by_id("ABTabPhone11").send_keys("8573182639")
+        wd.find_element_by_id("ABTabPhone11").send_keys(contact.Phone)
         wd.find_element_by_id("ABTabBirthdayDisplay1").click()
         wd.find_element_by_css_selector("button.button.gd_datepicker_dlg_display").click()
         wd.find_element_by_id("gd_datepicker_dlg_menu_year_2010").click()
         wd.find_element_by_xpath("//div[@id='gd_datepicker_dlg']/table/tbody/tr[5]/td[4]/div/div").click()
-        wd.find_element_by_xpath("//div[@class='docEditButtonbar']//button[.='Save and Exit']").click()
-        wd.find_element_by_id("anonymous_element_2").click()
-        self.assertTrue(success)
-    
+
+    def open_adresses(self, wd):
+        wd.find_element_by_link_text("Addresses").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_id("username").click()
+        wd.find_element_by_id("username").clear()
+        wd.find_element_by_id("username").send_keys(username)
+        wd.find_element_by_id("password").click()
+        wd.find_element_by_id("password").clear()
+        wd.find_element_by_id("password").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='signinbox']//button[.=' Secure Sign In']").click()
+
+    def open_home_page(self, wd):
+        wd.get("https://www.keepandshare.com/business/logout.php?i=2683987")
+
     def tearDown(self):
         self.wd.quit()
 
