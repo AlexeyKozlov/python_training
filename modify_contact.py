af852b052ae7ac198a28b812d62389954994141c
AlexeyKozlov <alexeykozlov@Alexeys-MBP.westell.com>
2016-10-23 19:57:13 -0400

    modification for deletion and modification tests: added test add group if no contacts exist

--- modify_contact.py
+++ modify_contact.py
@@ -1,8 +1,12 @@
 from model.contact import Contact
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Test"))
     app.contact.modify_first_contact(Contact(firstname="New"))
 
 
 def test_modify_contact_middlename(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Test"))
     app.contact.modify_first_contact(Contact(middlename="New"))
