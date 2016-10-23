6d2099b47509a2e9a0428a62bf86db412734e31f
AlexeyKozlov <alexeykozlov@Alexeys-MBP.westell.com>
2016-10-23 19:03:00 -0400

    one login and logout for all test

--- modify_contact.py
+++ modify_contact.py
@@ -1,12 +1,8 @@
 from model.contact import Contact
 
 def test_modify_contact_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New"))
-    app.session.logout()
 
 
 def test_modify_contact_middlename(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(middlename="New"))
-    app.session.logout()
--- test/add_contacts.py
+++ test/add_contacts.py
@@ -4,16 +4,12 @@ from model.contact import Contact
 
 
 def test_add_contacts(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Alex", middlename="Malex", lastname="Balex", company="Talex",
                                home="1234567890", mobile="0987654321", work="1234567899",
                                email="alexe@balex"))
-    app.session.logout()
 
 
 def test_add_empty_contacts(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", company="",
                                home="", mobile="", work="",
                                email=""))
-    app.session.logout()
