1b1b7884a46a78a56389a26c508d01c885be317a
AlexeyKozlov <alexeykozlov@Alexeys-MBP.westell.com>
2016-10-23 19:43:34 -0400

    intellectual login and logout

--- test/add_contacts.py
+++ test/add_contacts.py
@@ -8,7 +8,6 @@ def test_add_contacts(app):
                                home="1234567890", mobile="0987654321", work="1234567899",
                                email="alexe@balex"))
 
-
 def test_add_empty_contacts(app):
     app.contact.create(Contact(firstname="", middlename="", lastname="", company="",
                                home="", mobile="", work="",
